95b67683558c2a611693866857ab6f91eb3ea626
mark3gpk <mark.3g.pk@gmail.com>
2020-11-03 00:26:21 +0500

    basic project set and driver scaffold generated

---
+++ test/system/drivers_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+
+class DriversTest < ApplicationSystemTestCase
+  setup do
+    @driver = drivers(:one)
+  end
+
+  test "visiting the index" do
+    visit drivers_url
+    assert_selector "h1", text: "Drivers"
+  end
+
+  test "creating a Driver" do
+    visit drivers_url
+    click_on "New Driver"
+
+    fill_in "Car number", with: @driver.car_number
+    fill_in "Email", with: @driver.email
+    fill_in "License number", with: @driver.license_number
+    fill_in "Name", with: @driver.name
+    fill_in "Phone number", with: @driver.phone_number
+    click_on "Create Driver"
+
+    assert_text "Driver was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Driver" do
+    visit drivers_url
+    click_on "Edit", match: :first
+
+    fill_in "Car number", with: @driver.car_number
+    fill_in "Email", with: @driver.email
+    fill_in "License number", with: @driver.license_number
+    fill_in "Name", with: @driver.name
+    fill_in "Phone number", with: @driver.phone_number
+    click_on "Update Driver"
+
+    assert_text "Driver was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Driver" do
+    visit drivers_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Driver was successfully destroyed"
+  end
+end

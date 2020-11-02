d11a1686f29f58d535e6fa45005404f711f41768
mark3gpk <mark.3g.pk@gmail.com>
2020-11-03 03:21:38 +0500

    register and send_location APIs completed

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -1,8 +1,55 @@
-  class Api::V1::DriversController < ApplicationController
+class Api::V1::DriversController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
+  def register
+    driver = Driver.new driver_params
+    if driver.save
+      render json: { id: driver.id,
+                     name: driver.name,
+                     email: driver.email,
+                     phone_number: driver.phone_number,
+                     license_number: driver.license_number,
+                     car_number: driver.car_number
+                    }, status: 201
+    else
+      render json: { status: "failure",
+                     reason: driver.errors.full_messages
+                    }, status: 400
+    end
+  rescue StandardError => e # rescue if any exception occurs
+    render json: { status: "failure",
+                   reason: "#{e}"
+                  }, status: 400
+  end
+
+  def send_location
+    driver = Driver.where(id: params[:id]).first
+    if driver.present? && params[:latitude].present? && params[:longitude].present?
+      driver.latitude = params[:latitude]
+      driver.longitude = params[:longitude]
+      if driver.save
+        render json: { status: "success"
+                      }, status: 202
+      else
+        render json: { status: "failure",
+                       reason: driver.errors.full_messages
+                      }, status: 400
+      end
+    else
+      render json: { status: "failure",
+                     reason: "Either the id provided is wrong or something is missing in params"
+                    }, status: 400
+    end
+  rescue StandardError => e # rescue if any exception occurs
+    render json: { status: "failure",
+                   reason: "#{e}"
+                  }, status: 400
+  end
   
   private
     # Only allow a list of trusted parameters through.
     def driver_params
-      params.require(:driver).permit(:name, :email, :phone_number, :license_number, :car_number)
+      params.permit(:name, :email, :phone_number, :license_number, :car_number, :latitude, :longitude)
     end
 end
